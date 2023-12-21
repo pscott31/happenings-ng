06b030b46c2a35774e2ee57f9c74aced3c8b6bad
Philip Scott <phil@vega.xyz>
2023-12-21 13:57:09 +0000

    chore: switch to using binddeps instead of calling cargo from build.rs

--- build.rs
+++ build.rs
@@ -1,42 +1,38 @@
-use anyhow::{ensure, Context};
-use std::{env, path::PathBuf, process::{Command, Stdio}};
+use std::{env, path::PathBuf};
 use wasm_bindgen_cli_support::Bindgen;
 
-use jobserver::Client;
+// use anyhow::{ensure, Context};
+// use std::process::{Command, Stdio};
 
 fn main() -> anyhow::Result<()> {
     let out_dir = env::var("OUT_DIR").map(PathBuf::from)?;
-    let cargo = env::var("CARGO")?;
-    let fe_dir = out_dir.join("frontend");
     let debug = env::var("PROFILE")? == "debug";
 
-    println!("cargo:rerun-if-changed=frontend");
+    // let cargo = env::var("CARGO")?;
+    // let fe_dir = out_dir.join("frontend");
+    //     println!("cargo:rerun-if-changed=frontend");
+    //     let output = cmd
+    //         .arg("build")
+    //         .arg("--manifest-path=frontend/Cargo.toml")
+    //         .arg("--target=wasm32-unknown-unknown")
+    //         .arg("--target-dir")
+    //         .arg(fe_dir.as_os_str())
+    //         .stderr(Stdio::inherit())
+    //         .output()
+    //         .context("failed call `cargo` build for wasm")?;
 
-    // // See API documentation for why this is `unsafe`
-    // let jobserver =
-    //     match unsafe { Client::from_env() } {
-    //         Some(client) => client,
-    //         None => panic!("client not configured"),
-    //     };
+    //     ensure!(output.status.success(), "`cargo` invocation failed");
 
-    let jobserver = Client::new(8).expect("failed to create jobserver");
-    let mut cmd = Command::new(cargo);
-    jobserver.configure(&mut cmd);
+    // for (key, value) in env::vars() {
+    //     println!("cargo:warning {key}: {value}");
+    // }
 
-    let output = cmd
-        .arg("build")
-        .arg("--manifest-path=frontend/Cargo.toml")
-        .arg("--target=wasm32-unknown-unknown")
-        .arg("--target-dir")
-        .arg(fe_dir.as_os_str())
-        .stderr(Stdio::inherit())
-        .output()
-        .context("failed call `cargo` build for wasm")?;
-
-    ensure!(output.status.success(), "`cargo` invocation failed");
-
-    let wasm_in = fe_dir.join("wasm32-unknown-unknown/debug/frontend.wasm");
+    // let wasm_in = fe_dir.join("wasm32-unknown-unknown/debug/frontend.wasm");
+    let wasm_in = env::var("CARGO_CDYLIB_FILE_FRONTEND").map(PathBuf::from)?;
     let bindgen_out = out_dir.join("frontend_bg");
+
+    println!("cargo:warning=WASM INPUT: {wasm_in:?}");
+    println!("cargo:warning=BINDGEN OUT: {bindgen_out:?}");
     Bindgen::new()
         .input_path(wasm_in)
         .web(true)
